87bec6b10c55fe19efc5d4a5034202e95d4eb64c
KateSydor <katasydor@gmail.com>
2023-09-30 20:12:27 +0300

    Fixed build errors

--- server/src/test/java/com/example/webapplication/WebApplicationTests.java
+++ server/src/test/java/com/example/webapplication/WebApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.webapplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class WebApplicationTests {
 
     @Test
